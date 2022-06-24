cad1cbc69760af4d2140b6c2c56e19f4bfe86b51
Michael Gene Brockus <michaelbrockus@gmail.com>
2022-06-23 22:14:10 -0500

    use common header

--- code/common.h
+++ code/common.h
@@ -6,14 +6,22 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+//
+// standard C libraries used.
+//
 #include "ctype.h"
 #include "math.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
 
+//
+// the SDL2 library that is used the most
+//
 #include "SDL.h"
 
+//
+// common defs, structs and util headers
 #include "defs.h"
 #include "structs.h"
 
